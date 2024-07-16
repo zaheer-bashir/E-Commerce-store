dd55b959490e397689d09660dc51c4589f1fc567
zaheer-bashir <zaheerkpr3@gmail.com>
2024-07-17 02:05:28 +0500

    Fix: Fix UI Issue in Cart Modal

--- src/components/navbar/cartModal.tsx
+++ src/components/navbar/cartModal.tsx
@@ -4,33 +4,33 @@ import React from "react";
 const CartModal = () => {
   const cartItems = false;
   return (
-    <div className= "w-max absolute p-4 rounded-md shadow-md bg-white top-12 right-0 flex flex-col gap-6 z-20">
+    <div className="w-max absolute p-4 rounded-md shadow-md bg-white top-12 right-0 flex flex-col gap-6 z-20">
       {cartItems ? (
         <div className="">Cart is Empty</div>
       ) : (
-        <div className="flex flex-row gap-8">
-        <Image
+        <div className="flex flex-row gap-8 h-32 ">
+          <Image
             src="/example-product.jpg"
             alt="Cart Product Image"
             width={72}
-            height={72}
+            height={172}
             className="object-cover rounded-md"
           />
-          <div className="flex flex-col justify-between w-full">
+          <div className="flex flex-col justify-between">
             {/* TOP */}
-            <div className="flex items-center justify-between gap-8">
+            <div className="flex flex-col">
               {/* TITLE */}
               <div className="flex items-center justify-center gap-8">
                 <h1 className="font-semibold">Project Title</h1>
                 <div className="p-1 bg-gray-50 rounded-sm">$49</div>
               </div>
-              {/* DESC */}
-              <div className="text-sm text-gray-500">avaliable</div>
+            <div className="text-sm text-gray-500">avaliable</div>
             </div>
+            {/* DESC */}
             {/* BOTTOM */}
             <div className="flex justify-between text-sm">
-                <span className="text-gray-500">Qty. 2</span>
-                <span className="text-blue-500">Remove</span>
+              <span className="text-gray-500">Qty. 2</span>
+              <span className="text-blue-500">Remove</span>
             </div>
           </div>
         </div>
