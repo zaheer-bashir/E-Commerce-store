c38716a7797c2bc687c09d44a332ad35a26d370e
zaheer-bashir <zaheerkpr3@gmail.com>
2024-07-20 00:21:26 +0500

    Intial Setup of footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="">
+      {/*  TOP */}
+      <div className="">
+        {/* LEFT */}
+        <div className="w-1/4"></div>
+        {/* CENTER */}
+        <div className="w-1/4"></div>
+        {/* RIGHT */}
+        <div className="w-1/4"></div>
+      </div>
+      {/* BOTTOM */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Footer;
